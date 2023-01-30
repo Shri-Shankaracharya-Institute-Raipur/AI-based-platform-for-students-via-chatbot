9f39261ebd2c3ddd594b00e091b21492d70cc04d
harshita270102 <69470444+harshita270102@users.noreply.github.com>
2023-01-30 21:12:41 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,58 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.conversation import Statement
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+# creating a chatbot
+myBot = ChatBot(
+    name = 'Sakura',
+    read_only = True,
+    logic_adapters = [
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.BestMatch'
+    ]
+)
+
+# training the chatbot
+small_convo = [
+    'Hi there!',
+    'Hi',
+    'How do you do?',
+    'How are you?',
+    'I\'m cool.',
+    'Always cool.',
+    'I\'m Okay',
+    'Glad to hear that.',
+    'I\'m fine',
+    'I feel awesome',
+    'Excellent, glad to hear that.',
+    'Not so good',
+    'Sorry to hear that.',
+    'What\'s your name?',
+    ' I\'m Sakura. Ask me a math question, please.'
+]
+
+math_convo_1 = [
+    'Pythagorean theorem',
+    'a squared plus b squared equals c squared.'
+]
+
+math_convo_2 = [
+    'Law of Cosines',
+    'c**2 = a**2 + b**2 - 2*a*b*cos(gamma)'
+]
+
+list_trainee = ListTrainer(myBot)
+for i in (small_convo, math_convo_1, math_convo_2):
+    list_trainee.train(i)
+
+# using the ChatterBotCorpusTrainer class
+corpus_trainee = ChatterBotCorpusTrainer(myBot)
+corpus_trainee.train('chatterbot.corpus.english')
+
+print("Hi, I am ChatBot")
+
+while True:
+    query = input(">>>")
+    print(myBot.get_response(Statement(text=query, search_text=query)))
